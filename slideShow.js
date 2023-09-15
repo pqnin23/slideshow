ace34dcd65e881dc13e4eb19d8a2f5e5f677915f
pqnin23 <128159590+pqnin23@users.noreply.github.com>
2023-09-15 11:08:25 -0300

    Add files via upload

--- slideShow.js
+++ slideShow.js
@@ -9,12 +9,29 @@ const imagens =[
     {'id': '6', 'url': './img/yuyuhakusho.jpg'},
 ]
 const containerItens = document.querySelector('#container-itens');
-const loadImages =(images,container) =>{
-    images.forEach(image =>{
+
+const loadImages =(images, container) =>{
+    images.forEach(image => {
         container.innerHTML += `<div class='item'>
         <img src='${image.url}'>
             </div>
         `
-    })
-    
+      })  
     }
+    loadImages(images, containersItens);
+
+    let itens = document.querySelectorAll('.item');
+
+    const previous = () => {
+        containerItens.appendChild(itens[0]);
+        itens = document.querySelectorAll('.item');
+    }
+
+    const next = () => {
+        const lastItem = itens[itens.length - 1];
+        containerItens.insertBefore( lastItem, itens[0] );
+        itens = document.querySelectorAll('.item');
+    }
+
+    document.querySelectorAll('#previous').addEventListener('click', previous);
+    document.querySelectorAll('#next').addEventListener('click', next);
